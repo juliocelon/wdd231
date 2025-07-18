617a6de4320e58585df4784e7b152c53d2a9b8f1
Julio Celon <grolain@hotmail.com>
2025-07-17 18:32:17 -0600

    W03 Assignment: Chamber Home Page

--- chamber/scripts/members2or3.js
+++ chamber/scripts/members2or3.js
@@ -2,56 +2,6 @@
 
 const cards = document.querySelector('#bussiness');
 
-async function getBussinessData() {
-
-    const response = await fetch("https://juliocelon.github.io/wdd231/chamber/data/members.json"); // request
-    if (!response.ok) {
-        console.error("HTTP error:", response.status);
-        return;
-    }
-    const data = await response.json();
-    console.table(data.members);
-    displayMembers(data.members);
-}
-
-//getBussinessData();
-
-const displayMembers = (members) => {
-    members.forEach((member) => {
-        let card = document.createElement('section');
-
-        let name = document.createElement('h3');
-        name.textContent = member.name;
-
-        let contactInfo = document.createElement('div');
-        contactInfo.innerHTML = `
-            <p>${member.address}</p>
-            <p>${member["phone number"]}</p>
-        `;
-
-        let website = document.createElement('a');
-        website.href = member["web site url"];
-        website.textContent = "Visit Website";
-        website.target = "_blank";
-
-        // Image (only for grid)
-        let image = document.createElement('img');
-        image.src = member.image;
-        image.alt = `Logo of ${member.name}`;
-        image.loading = 'lazy';
-
-        // Append for grid
-        card.appendChild(image);
-        card.appendChild(name);
-        card.appendChild(contactInfo);
-        card.appendChild(website);
-
-        cards.appendChild(card);
-    });
-};
-
-// For index.html
-
 async function getBussinessDataLevel2or3() {
 
     const response = await fetch("https://juliocelon.github.io/wdd231/chamber/data/members.json"); // request
@@ -61,7 +11,7 @@ async function getBussinessDataLevel2or3() {
     }
     const data = await response.json();
     console.table(data.members);
-    displayMembers(data.members);
+    displayMembersLevel2or3(data.members);
 }
 
 getBussinessDataLevel2or3();
@@ -72,7 +22,11 @@ const displayMembersLevel2or3 = (members) => {
         member["membership level"] === 2 || member["membership level"] === 3
     );
 
-    filteredMembers.forEach((member) => {
+    const shuffledMembers = filteredMembers.sort(() => 0.5 - Math.random());
+
+    const selectedMembers = shuffledMembers.slice(0, 3);
+
+    selectedMembers.forEach((member) => {
         let card = document.createElement('section');
 
         let name = document.createElement('h3');
