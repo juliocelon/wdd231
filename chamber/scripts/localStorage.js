e4241d6bdc8657530b30ca9d959cca0bf6bf8e2a
Julio Celon <grolain@hotmail.com>
2025-08-01 07:37:53 -0600

    W05: Chamber Discover Page

---
+++ chamber/scripts/localStorage.js
@@ -0,0 +1,59 @@
+/* getdates.js */
+
+const currentyear = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+
+const today = new Date();
+
+currentyear.innerHTML = `${today.getFullYear()}`;
+
+let lastModification = new Date(document.lastModified);
+
+const month = String(lastModification.getMonth() + 1).padStart(2, '0');
+const day = String(lastModification.getDate()).padStart(2, '0');
+
+const hours = String(lastModification.getHours()).padStart(2, '0');
+const minutes = String(lastModification.getMinutes()).padStart(2, '0');
+const seconds = String(lastModification.getSeconds()).padStart(2, '0');
+
+lastModified.innerHTML = `Last modification:${day}/${month}/${lastModification.getFullYear()} ${hours}:${minutes}:${seconds}`;
+
+/* app.js */
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
+});
+
+
+/* */
+
+const paragraph = document.getElementById('visitMessage');
+const lastVisit = localStorage.getItem('lastVisit');
+const now = Date.now();
+let message = "";
+
+if (!lastVisit) {
+    // First visit
+    message = "Welcome! Let us know if you have any questions.";
+} else {
+    const millisecondsInADay = 24 * 60 * 60 * 1000;
+    const daysSinceLastVisit = Math.floor((now - parseInt(lastVisit)) / millisecondsInADay);
+
+    if (daysSinceLastVisit < 1) {
+        message = "Back so soon! Awesome!";
+    } else if (daysSinceLastVisit === 1) {
+        message = "You last visited 1 day ago.";
+    } else {
+        message = `You last visited ${daysSinceLastVisit} days ago.`;
+    }
+}
+
+// Update paragraph content
+paragraph.textContent = message;
+
+// Store current date as last visit
+localStorage.setItem('lastVisit', now);
