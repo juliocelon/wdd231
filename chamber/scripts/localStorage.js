3f092bb72b68792da53db6b02c14f22a8e5dd8f5
Julio Celon <grolain@hotmail.com>
2025-08-01 07:44:33 -0600

    fixes

--- chamber/scripts/localStorage.js
+++ chamber/scripts/localStorage.js
@@ -1,35 +1,3 @@
-/* getdates.js */
-
-const currentyear = document.querySelector("#currentyear");
-const lastModified = document.querySelector("#lastModified");
-
-const today = new Date();
-
-currentyear.innerHTML = `${today.getFullYear()}`;
-
-let lastModification = new Date(document.lastModified);
-
-const month = String(lastModification.getMonth() + 1).padStart(2, '0');
-const day = String(lastModification.getDate()).padStart(2, '0');
-
-const hours = String(lastModification.getHours()).padStart(2, '0');
-const minutes = String(lastModification.getMinutes()).padStart(2, '0');
-const seconds = String(lastModification.getSeconds()).padStart(2, '0');
-
-lastModified.innerHTML = `Last modification:${day}/${month}/${lastModification.getFullYear()} ${hours}:${minutes}:${seconds}`;
-
-/* app.js */
-
-const hamButton = document.querySelector("#menu");
-const navigation = document.querySelector(".navigation");
-
-hamButton.addEventListener("click", () => {
-    navigation.classList.toggle("open");
-    hamButton.classList.toggle("open");
-});
-
-
-/* */
 
 const paragraph = document.getElementById('visitMessage');
 const lastVisit = localStorage.getItem('lastVisit');
